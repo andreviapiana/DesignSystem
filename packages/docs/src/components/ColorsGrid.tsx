89d7beec989b497b7aeeb908a7ba92bb2ab2f966
André Viapiana <andreviapiana@gmail.com>
2023-04-03 10:50:45 -0300

    🔨refactor: Renomeando o Projeto (p/ coincidir com meu NPM)

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,4 @@
-import { colors } from '@ignite-ui/tokens'
+import { colors } from '@ignite-andre/tokens'
 import { getContrast } from 'polished'
 
 export function ColorsGrid() {
--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@ignite-andre/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
